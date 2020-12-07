67b1f4de52332602d230be62a6faef3971ac6e79
JoshBernstein <josh.bern@gmail.com>
2020-12-06 20:51:10 -0500

    submitting exercise2

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+#Multiply an integer by a float
+
+puts 55 * 0.2
+
+#Add a string and an integer
+
+puts 10 + "10".to_i
+
+#Output calculated results in a string using string interpolation
+
+puts "The result of 45628 * 7839 is #{45628 * 7839}"
+
+#Combining comparison and logical operators
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
