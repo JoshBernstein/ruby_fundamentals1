c4c515f87757551d1e4ebe9c0e34186d0e6dda53
JoshBernstein <josh.bern@gmail.com>
2020-12-06 21:20:02 -0500

    completed exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,5 @@
 puts "What is your name?"
+user_name = gets.chomp
+puts "Hello, #{user_name}. How old are you?"
+user_age = gets.chomp.to_i
+puts "Wow #{user_name}, that means you were born in #{2020-user_age}!"
